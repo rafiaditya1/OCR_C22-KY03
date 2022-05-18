b651c3bbeaab59b309d53bd0f0361af1399e367b
erzajuan <direkal2@gmail.com>
2022-05-18 16:12:28 +0700

    Membuat MainActivity.kt

--- app/src/main/java/com/bangkit/ocr_c22_ky03/MainActivity.kt
+++ app/src/main/java/com/bangkit/ocr_c22_ky03/MainActivity.kt
@@ -2,10 +2,22 @@ package com.bangkit.ocr_c22_ky03
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.bangkit.ocr_c22_ky03.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnAdd.setOnClickListener{
+
+        }
+        binding.btnStatus.setOnClickListener{
+
+        }
     }
 }
